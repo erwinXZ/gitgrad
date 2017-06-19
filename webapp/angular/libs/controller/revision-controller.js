1225cc27634482cf617a9f9128a1088abbab9ec3
kaya_1996 <kaya@kaya-HP-ENVY-14-Notebook-PC>
2017-06-19 00:45:28 -0400

    El pollito pio

--- webapp/angular/libs/controller/revision-controller.js
+++ webapp/angular/libs/controller/revision-controller.js
@@ -1,9 +1,16 @@
 var app = angular.module('gitGradApp.revisionCtrl', ["ngStorage"]);
 
 // controlador clientes
-app.controller('revisionCtrl', ['$scope','$window','loginServices','$sessionStorage', function($scope,$window,loginServices,$sessionStorage){
+app.controller('revisionCtrl', ['$scope','$window','revisionServices','$sessionStorage', function($scope,$window,loginServices,$sessionStorage){
 	
     $scope.revision = "Revision";
     $scope.proyecto = $sessionStorage.dataProyect ;
+    $scope.listarDocumentos = function(id){
+        revisionServices.listarDocumentos(id).then(function(){
+            console.log(revisionServices.response.message);
+        });
+    }
+
+    $scope.listarDocumentos($scope.proyecto.id);
 
 }]) 
--- webapp/angular/libs/services/revision-service.js
+++ webapp/angular/libs/services/revision-service.js
@@ -3,12 +3,12 @@ var app = angular.module('gitGradApp.revisionServices',[])
 app.factory('revisionServices', ['$http','$q','$rootScope', function($http,$q,$rootScope){
 
 var self ={
-				listarProyectos : function(id){
+				listarDocumentos : function(id){
 					var d = $q.defer();
 					// console.log(datos);
                     $http({
                       method: 'GET',
-					  	url: 'http://192.168.1.7/gitgrad/api/public/project/projectsList/'+id,
+					  	url: 'http://192.168.1.7/gitgrad/api/public/document/docsList/'+id,
                     	})
                         .then(function successCallback(response) {
 								self.response 	= response.data;
@@ -20,9 +20,6 @@ var self ={
                        return d.promise;	 
 	
 				}
-
 	}
-
-
 	return self;
 }])
