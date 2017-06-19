6644f0735c4672b8a62eff4e7288a42887015905
kaya_1996 <kaya@kaya-HP-ENVY-14-Notebook-PC>
2017-06-19 08:55:41 -0400

    El pollito pio 3

--- webapp/angular/libs/controller/revision-controller.js
+++ webapp/angular/libs/controller/revision-controller.js
@@ -1,16 +1,16 @@
 var app = angular.module('gitGradApp.revisionCtrl', ["ngStorage"]);
 
 // controlador clientes
-app.controller('revisionCtrl', ['$scope','$window','revisionServices','$sessionStorage', function($scope,$window,loginServices,$sessionStorage){
+app.controller('revisionCtrl', ['$scope','$window','revisionServices','$sessionStorage', function($scope,$window,revisionServices,$sessionStorage){
 	
     $scope.revision = "Revision";
     $scope.proyecto = $sessionStorage.dataProyect ;
     $scope.listarDocumentos = function(id){
         revisionServices.listarDocumentos(id).then(function(){
             console.log(revisionServices.response.message);
+            $scope.documentos = revisionServices.response.message;
         });
     }
-
     $scope.listarDocumentos($scope.proyecto.id);
 
 }]) 
