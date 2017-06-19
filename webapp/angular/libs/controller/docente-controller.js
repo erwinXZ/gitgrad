c176a47b0462a3db30f28c51a2d4a7e09b8b7ce3
benji Castillo <castilloeguezbenji@gmail.com>
2017-06-18 21:44:24 -0400

    vista proyectos/docente

--- webapp/angular/libs/controller/docente-controller.js
+++ webapp/angular/libs/controller/docente-controller.js
@@ -3,7 +3,10 @@ var app = angular.module('gitGradApp.docenteCtrl', ["ngStorage"]);
 // controlador clientes
 app.controller('docenteCtrl', ['$scope','$window','docenteServices','$sessionStorage', function($scope,$window,docenteServices,$sessionStorage){
 	 $scope.docente = $sessionStorage.data;
-
+     $scope.proyectoInsertar = {}
+     $scope.estudiante1 = false;
+     $scope.estudiante2 = false;
+     
      $scope.listarProyectos = function(id){
         docenteServices.listarProyectos(id).then(function(){
             console.log(docenteServices.response.message);
@@ -19,5 +22,25 @@ app.controller('docenteCtrl', ['$scope','$window','docenteServices','$sessionSto
         $sessionStorage.dataProyect = proyecto;
         $window.location.href = '#/docente/revision';
      
+    }
+    $scope.insertarProyecto = function(proyecto){
+        $scope.proyectoInsertar = proyecto;
+        $scope.proyectoInsertar.id_docente = $scope.docente.id;
+        console.log($scope.proyectoInsertar);
+
+            docenteServices.insertarProyecto($scope.proyectoInsertar).then(function(){
+                console.log(docenteServices.response.message);
+                if(docenteServices.response.message == 0){
+                       $scope.estudiante1 = true;
+                       $scope.estudiante2 = false;
+                }
+                if(docenteServices.response.message == 1){
+                       $scope.estudiante2 = true;
+                       $scope.estudiante1 = false;
+                }
+                // $scope.proyectos = docenteServices.response.message;
+            });        
+        
+        
     }
 }]) 
--- webapp/angular/libs/services/docente-service.js
+++ webapp/angular/libs/services/docente-service.js
@@ -19,6 +19,38 @@ var self ={
                         });
                        return d.promise;	 
 	
+				},
+				insertarProyecto : function(datos){
+					var d = $q.defer();
+					console.log(datos);
+                    $http({
+                      method: 'POST',
+					  	url: 'http://192.168.1.7/gitgrad/api/public/project/',
+                        // url: 'http://localhost/gitgrad/APIPOLLO/public/observation/read/',
+                        data:{
+								_name:datos.name,
+								_modality:datos.modality,
+								_gestion:datos.gestion,
+								_cu:datos.cu,
+								_id_teacher:datos.id_docente
+						}
+                    	})
+                        .then(function successCallback(response) {
+                                // ok
+                                // self.cargado		= true;
+    							// self.cargando		= false;
+								self.response 	= response.data;
+								
+								return d.resolve()	
+                            }, function errorCallback(response) {
+                            // ko
+                            	return d.resolve()	
+                                // self.cargado		= true;
+    							// self.cargando		= false;
+								self.response 	= response.data
+                        });
+                       return d.promise;	 
+	
 				}
 
 	}
